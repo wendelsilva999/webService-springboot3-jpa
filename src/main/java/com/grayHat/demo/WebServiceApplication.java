9a5574f9744ef8bde5df343d330dbcfa5afc385c
staz <wendelsilva043@gmil.com>
2024-01-31 07:58:35 -0300

    Project created

---
+++ src/main/java/com/grayHat/demo/WebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.grayHat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grayHat/demo/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grayHat.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
